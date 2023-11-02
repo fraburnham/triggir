8495246b595745057b4813bf800728fee8be48ca
Frank Burnham <frank@n8git.dev>
2023-11-02 11:22:40 -0400

    Move workdir logic into elixir

--- lib/triggir_web/controllers/gitlab_webhook_controller.ex
+++ lib/triggir_web/controllers/gitlab_webhook_controller.ex
@@ -35,12 +35,51 @@ defmodule TriggirWeb.GitlabWebhookController do
   end
 
   defp envify_map({k, v}, prefix) do
-    # gotta be a better way to handle numbers...
     [{build_key(k, prefix), "#{v}"}]
   end
 
   defp build_context(request_body, workdir) do
     %{"env" => envify_map(request_body |> Map.put("WORKDIR", workdir))}
+  end
+
+  def get_run_numbers(paths, sha) do
+    paths
+    |> Stream.filter(fn el ->
+      String.starts_with?(el, sha)
+    end)
+    |> Stream.map(fn el ->
+      [_, num] = String.split(el, "-")
+      num
+    end)
+    |> Enum.sort(:desc)
+  end
+
+  def get_last_run_number(project_dir, sha) do
+    case File.ls(project_dir) do
+      {:ok, paths} ->
+        case get_run_numbers(paths, sha) do
+          [run_num | _] -> String.to_integer(run_num)
+          _ -> 0
+        end
+
+      {:error, _} ->
+        0
+    end
+  end
+
+  def get_workdir(trigger, project, sha) do
+    project_dir =
+      Enum.reduce(
+        [Application.fetch_env!(:triggir, :runs_path), trigger, project],
+        fn path, acc ->
+          Path.join(acc, path)
+        end
+      )
+
+    workdir_base = Path.join(project_dir, sha)
+    next_num = get_last_run_number(project_dir, sha) + 1
+
+    "#{workdir_base}-#{next_num}"
   end
 
   def store_results(results, workdir) do
@@ -59,18 +98,8 @@ defmodule TriggirWeb.GitlabWebhookController do
   end
 
   def run(conn, request_body) do
-    # Work needs to happen in a separate process eventually. For experimentation this is probably fine.
-    base_dir = Application.fetch_env!(:triggir, :runs_path)
-    project = request_body["repository"]["name"]
-    sha = request_body["checkout_sha"]
-
     workdir =
-      Enum.reduce(
-        [base_dir, "gitlab", project, sha],
-        fn path, acc ->
-          Path.join(acc, path)
-        end
-      )
+      get_workdir("gitlab", request_body["repository"]["name"], request_body["checkout_sha"])
 
     setup_results = Taskir.main(build_context(request_body, workdir), "tasks/checkout.yaml")
 
